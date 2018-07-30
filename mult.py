88bd4c8a3c7d666f67c5ab2f7893bb5461e32145
AnnaLitvinova79 <41925496+AnnaLitvinova79@users.noreply.github.com>
2018-07-30 12:49:57 -0700

    Print multiplication table in python3

---
+++ mult.py
@@ -0,0 +1,14 @@
+def print3(x):
+	print(x,' ',end='')
+	if x <= 9:
+		print(' ',end='')
+	
+	
+def foo(start, end):
+	for i in range(start,end+1,1):
+		for j in range(start,end+1,1):
+			print3(i* j)
+		print("")
+
+foo(1,9)
+#input("Press enter to continue")
